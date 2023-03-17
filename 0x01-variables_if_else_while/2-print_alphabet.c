707d61bb0a5f2593b20ffd03b95ba4a755e2d99c
achrafelaffas <achrafelaffas@gmail.com>
2023-03-17 13:13:37 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+
+/**
+ * main - Entry point
+ * Description: if-else program.
+ * Return: (0) always - Success.
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while(ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	return (0);
+}
