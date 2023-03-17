f8a97eda9df54b2064f4f032595d054014b82f79
achrafelaffas <achrafelaffas@gmail.com>
2023-03-17 14:16:38 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+
+/**
+ * main - Entry point
+ * Description: if-else program.
+ * Return: (0) always - Success.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}	
+
+	putchar('\n');
+
+	return (0);
+}
