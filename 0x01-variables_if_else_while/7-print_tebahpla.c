a39e84762ae3ea7e580000339cb634ee0070037e
achrafelaffas <achrafelaffas@gmail.com>
2023-03-17 13:52:24 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+
+/**
+ * main - Entry point
+ * Description: if-else program.
+ * Return: (0) always - Success.
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
