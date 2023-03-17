8e7d4af50438dab56eb100b07f57e5f5896c5bf6
achrafelaffas <achrafelaffas@gmail.com>
2023-03-17 14:02:56 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+
+/**
+ * main - Entry point
+ * Description: if-else program.
+ * Return: (0) always - Success.
+ */
+int main(void)
+{
+	int i;
+	char ch = 'a';
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+
+	while (ch <= 'f')
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
