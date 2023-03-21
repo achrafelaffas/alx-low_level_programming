228558a75d50fad8aba7e7af503577b08a603db4
achrafelaffas <achrafelaffas@gmail.com>
2023-03-21 14:55:38 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * isSlower - Is the char in lowercase
+  *
+  * Return: 1 (sucess)
+  * Return: 0 (failure)
+  */
+
+int _islower(int c)
+{
+	if (96 < c < 123)
+		return (1);
+	else
+		return (0);
+}
