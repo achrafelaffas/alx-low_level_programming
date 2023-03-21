23b105e3f4d5635b1ab73d5df4292192055b3c9a
achrafelaffas <achrafelaffas@gmail.com>
2023-03-21 15:57:01 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * jack_bauer- prints every minute of the day
+  *
+  * Return: void
+  */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		_putchar(i);
+		_putchar(i);
+		for(j = 0; j <= 59; j++)
+		{
+			_putchar(58);
+			putchar(j);
+			putchar(j);
+		}
+	}
+}
