da8736fbff471f7962a90cd3ae7b5698a90a0bac
achrafelaffas <achrafelaffas@gmail.com>
2023-03-23 10:10:54 -0700

    0 to 9 - numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		for(j = '0' ; <= '14'; j++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
