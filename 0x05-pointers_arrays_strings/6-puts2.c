2bad2d11be65cc023d207d1d33a50bffd3188497
achrafelaffas <achrafelaffas@gmail.com>
2023-03-27 10:46:35 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *puts2 -  prints every other character of a string,
+ *starting with the first character,
+ *followed by a new line.
+ *
+ *@str: stirng pointer
+ *
+ *Return: void
+ */
+
+void puts2(char *str)
+{
+	_putchar('a');
+
+}
+
+
