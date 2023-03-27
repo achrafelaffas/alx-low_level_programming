8d2217092b352cbd352a467a64d08591c9a7a758
achrafelaffas <achrafelaffas@gmail.com>
2023-03-27 09:42:08 -0700

    print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,21 +3,21 @@
 /**
  * print_rev - print a string in reverse.
  *
- * @a: pointer to string
+ * @s: pointer to string
  *
  * Return: void
  */
 
 void print_rev(char *s)
 {
-	char* end = s;
+	char *end = s;
 
 	while (*end != '\0')
 		end++;
 
 	end--;
 
-	while(end >= s)
+	while (end >= s)
 	{
 		_putchar(*end);
 		end--;
