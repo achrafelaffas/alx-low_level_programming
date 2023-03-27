c13bf90b55785f81961707fb3a3df622f25b8a40
achrafelaffas <achrafelaffas@gmail.com>
2023-03-27 12:33:02 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,36 @@
  * Return: void
  */
 
-void puts_half(char* str) {
-	int length = strlen(str);
-	int half = (length + 1) / 2;
-	int i;
 
-	for (i = half; i < length; i++)
+void puts_half(char *str)
+{
+	char *end = str;
+	char *start = str;
+	int length = 0;
+
+	while (*end != '\0')
 	{
-		_putchar(str[i]);
+		end++;
+		length++;
 	}
 
-	_putchar('\n');
+	int half = (length + 1) / 2;
+
+	if (length % 2 != 0)
+	{
+		start += half - 1;
+	}
+	else
+	{
+		start += half;
+	}
+
+	while (*start != '\0')
+	{
+		putchar(*start);
+		start++;
+	}
+
+	putchar('\n');
 }
+
