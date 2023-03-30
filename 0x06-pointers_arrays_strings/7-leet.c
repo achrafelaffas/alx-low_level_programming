def638bd03880445e42cbf9435a9e3367b4aebcc
achrafelaffas <achrafelaffas@gmail.com>
2023-03-29 18:53:30 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,46 +2,30 @@
 
 /**
  * leet - encodes a string into 1337
- * @str: pointer to string
+ * @s: pointer to string
  *
  * Return: pointer to an encoded string
  */
 
-char *leet(char *str)
+char *leet(char *s)
 {
-	char *ptr = str;
-	char c;
+	int stringCount, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
 
-	while (*ptr != '\0')
+	stringCount = 0;
+	while (s[stringCount] != '\0')
 	{
-		if ((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z'))
+		leetCount = 0;
+		while (leetCount < 10)
 		{
-			c = (*ptr >= 'a' && *ptr <= 'z') ? (*ptr - 'a' + 'A') : *ptr;
-			if (c == 'A' || c == 'a')
+			if (leetLetters[leetCount] == s[stringCount])
 			{
-				*ptr = '4';
+				s[stringCount] = leetNums[leetCount];
 			}
-			else if (c == 'E' || c == 'e')
-			{
-				*ptr = '3';
-			}
-			else if (c == 'O' || c == 'o')
-			{
-				*ptr = '0';
-			}
-			else if (c == 'T' || c == 't')
-			{
-				*ptr = '7';
-			}
-			else if (c == 'L' || c == 'l')
-			{
-				*ptr = '1';
-			}
-			else
-			{
-			}
+			leetCount++;
 		}
-		ptr++;
+		stringCount++;
 	}
-	return (str);
+	return (s);
 }
