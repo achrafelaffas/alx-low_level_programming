961fe99e54738b68099d6a072944e630f65975ed
achrafelaffas <achrafelaffas@gmail.com>
2023-04-09 11:02:22 -0700

    add

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,63 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-
 /**
-* min_coins - Calculates the minimum number of coins required to make change
-* for an amount of money.
-* @amount: The amount of money to make change for.
-*
-* Return: The minimum number of coins required.
-*/
-int min_coins(int amount)
-{
-	int coins = 0;
-	int coin_values[] = {25, 10, 5, 2, 1};
-	int num_coin_values = sizeof(coin_values) / sizeof(coin_values[0]);
+ * main - prints the minimum number of coins 
+ * to make change for an amount of money
+ * @argc: length of argv
+ * @argv: number of argument
+ * Return: Always 0
+ */
 
-	for (int i = 0; i < num_coin_values; i++)
-	{
-		int coin = coin_values[i];
-
-		while (amount >= coin)
-		{
-			amount -= coin;
-			coins++;
-		}
-	}
-
-	return (coins);
-}
-
-/**
-* main - Entry point. Calculates the minimum number of coins required to make
-* change for an amount of money supplied as a command-line argument.
-* @argc: The number of command-line arguments.
-* @argv: An array of command-line argument strings.
-*
-* Return: 0 if successful, 1 if an incorrect number of command-line arguments
-* is supplied.
-*/
 int main(int argc, char *argv[])
 {
+	int position, total, change, aux;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	position = total = change = aux = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int amount = atoi(argv[1]);
-
-	if (amount < 0)
+	total = atoi(argv[1]);
+	if (total <= 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	int num_coins = min_coins(amount);
-
-	printf("%d\n", num_coins);
-
+	while (coins[position] != '\0')
+	{
+		if (total >= coins[position])
+		{
+			aux = (total / coins[position]);
+			change += aux;
+			total -= coins[position] * aux;
+		}
+		position++;
+	}
+	printf("%d\n", change);
 	return (0);
 }
-
