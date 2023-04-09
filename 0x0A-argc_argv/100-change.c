4c756a3c6f5888a921afe1966435278a65489ce5
achrafelaffas <achrafelaffas@gmail.com>
2023-04-09 10:47:05 -0700

    add

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* min_coins - Calculates the minimum number of coins required to make change
+* for an amount of money.
+* @amount: The amount of money to make change for.
+*
+* Return: The minimum number of coins required.
+*/
+int min_coins(int amount)
+{
+	int coins = 0;
+	int coin_values[] = {25, 10, 5, 2, 1};
+	int num_coin_values = sizeof(coin_values) / sizeof(coin_values[0]);
+
+	for (int i = 0; i < num_coin_values; i++)
+	{
+		int coin = coin_values[i];
+
+		while (amount >= coin)
+		{
+			amount -= coin;
+			coins++;
+		}
+	}
+
+	return (coins);
+}
+
+/**
+* main - Entry point. Calculates the minimum number of coins required to make
+* change for an amount of money supplied as a command-line argument.
+* @argc: The number of command-line arguments.
+* @argv: An array of command-line argument strings.
+*
+* Return: 0 if successful, 1 if an incorrect number of command-line arguments
+* is supplied.
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int amount = atoi(argv[1]);
+	int num_coins = min_coins(amount);
+
+	printf("%d\n", num_coins);
+
+	return (0);
+}
+
