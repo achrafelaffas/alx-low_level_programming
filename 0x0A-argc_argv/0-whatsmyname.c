51dddd90f84febddb019dc69c70386642f5aa41e
achrafelaffas <achrafelaffas@gmail.com>
2023-04-08 18:24:41 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * @argc: number of passed arguments
+ * @argv: 
+ *
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
