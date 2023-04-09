c4a588c7ce0b60b0392b9b3713fedc64f484011c
achrafelaffas <achrafelaffas@gmail.com>
2023-04-09 09:19:22 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,16 +5,14 @@
 /**
  * main - mul. of two integers
  *
- * @argv: array of args
  * @argc: number of args
+ * @argv: array of args
  *
  * Return: 0 - success, 1 - error.
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argc;
-
 	if (argc == 3)
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
