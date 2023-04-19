352dc0e7d4705d29594af9133ca22bf09d0a5638
achrafelaffas <achrafelaffas@gmail.com>
2023-04-19 15:56:30 -0700

    find int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,13 +19,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	if (array == NULL || cmp == NULL || size <= 0)
 	{
-		exit(EXIT_FAILURE);
-	}
-
-	if (size <= 0)
 		return (-1);
+	}
 
 	for (i = 0; i < size; i++)
 	{
