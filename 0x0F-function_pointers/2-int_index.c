cc514ca81864662886eb75dd9036e2147e5e3cbe
achrafelaffas <achrafelaffas@gmail.com>
2023-04-19 15:54:19 -0700

    find int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,37 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - looks for an integer
+ *
+ * @array: pointer to an array of elements
+ * @size: the size of the array
+ *
+ * @cmp: pointer to a function
+ *
+ * Return: the index of the first element
+ * for which the cmp function does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(*(array + x))
+			return (x);
+	}
+
+	return (-1);
+}
