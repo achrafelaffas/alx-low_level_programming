5515f344f51981494715069029a07bf0266661d8
achrafelaffas <achrafelaffas@gmail.com>
2023-04-20 15:18:04 -0700

    print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,14 +4,19 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_int(va_list list);
+void print_float(va_list list);
+void print_char(va_list list);
+void print_str(va_list list);
+
 /**
- * struct format_types - Struct format_types
- * @identifier: The conversion specifier
- * @f: The function pointer
+ * struct printTypeStruct - structure definition of a printTypeStruct
+ * @type: type
+ * @printer: function to print
  */
-typedef struct format_types
+typedef struct printTypeStruct
 {
-	char *identifier;
-	void (*f)(char *separator, va_list args);
-} f_dt;
+char *type;
+void (*printer)(va_list);
+} printTypeStruct;
 #endif
