0d74da4c1cfbccf7415dfebde42707a70e0ddc77
achrafelaffas <achrafelaffas@gmail.com>
2023-04-27 22:52:45 -0700

    modf

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,21 +9,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *node;
 	size_t n;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
+	node->str = strdup(str);
 
 	for (n = 0; str[n]; n++)
 		;
 
-	new_node->len = n;
-	new_node->next = *head;
-	*head = new_node;
+	node->len = n;
+	node->next = *head;
+	*head = node;
 
 	return (*head);
 }
