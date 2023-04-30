37926b49e769b05afaffe8ce029da2a5824d6c2e
achrafelaffas <achrafelaffas@gmail.com>
2023-04-30 02:21:11 -0700

    print list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-    listint_t *temp;
+	listint_t *crr;
 
-    while (head)
-    {
-        temp = head->next;
-        free(head);
-        head = temp;
-    }
+	while (head)
+	{
+		crr = head->next;
+		free(head);
+		head = crr;
+	}
 }
