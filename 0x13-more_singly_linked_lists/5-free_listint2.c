ddef4eacd5973310985bb50ec5edb0310546a51a
achrafelaffas <achrafelaffas@gmail.com>
2023-04-30 04:06:45 -0700

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * free_listint2 -  frees a listint_t list
  * @head: pointer to the listint_t list to be freed
@@ -18,5 +18,4 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = crr;
 	}
-	*head = NULL;
 }
