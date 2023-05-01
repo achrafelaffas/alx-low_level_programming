2aff0dd60993acd523bb49053e0a49df226c704c
achrafelaffas <achrafelaffas@gmail.com>
2023-05-01 06:15:39 -0700

    free list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,20 +4,20 @@
  * free_listint2 -  frees a listint_t list
  * @head: pointer to the listint_t list to be freed
  */
-
 void free_listint2(listint_t **head)
 {
-	listint_t *mvn;
-	listint_t **temp = head;
+listint_t *temp;
+listint_t **node = head;
 
-	if (temp != NULL)
-	{
-		while (*head != NULL)
-		{
-			mvn = *head;
-			free(mvn);
-			*head = (*head)->next;
-		}
-		*temp = NULL;
-	}
+if (head == NULL)
+return;
+
+while (*head)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+
+*node = NULL;
 }
